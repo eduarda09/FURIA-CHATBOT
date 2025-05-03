f67154756cc7e177ee1f575573b9a8166f2409de
eduarda09 <m.eduardakl@gmail.com>
2025-05-03 20:10:54 -0300

    Resolvendo bugs de imagens

--- src/components/ChatBot/ChatBot.jsx
+++ src/components/ChatBot/ChatBot.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import ChatForm from '../ChatForm/ChatForm';
 import ChatMessage from '../ChatMessage/ChatMessage';
 import { useChat } from '../../context/ChatContext';
-
+import LogoImage from '../../assets/furia.png';
 
 const ChatBot = () => {
       const { isChatOpen, setIsChatOpen } = useChat();
@@ -58,7 +58,7 @@ const ChatBot = () => {
         <div className="chatbot-popup">
           <div className="chatbot-header">
             <div className="header-info">
-              <img className="logo" src="../../assets/furia.png" alt="" />
+              <img className="logo" src={LogoImage} alt="Image logo of FURIA" />
               <h2 className="logo-text">FURIA ChatBot</h2>
             </div>
             <button
--- src/components/HowToUse/HowToUse.jsx
+++ src/components/HowToUse/HowToUse.jsx
@@ -1,5 +1,7 @@
 import React, { useRef, useEffect} from 'react';
 import './HowToUse.css';
+import Video from '../../assets/chat-video.mp4';
+import Image from '../../assets/chat.jpg';
 
 
 
@@ -40,7 +42,7 @@ const HowToUse = () => {
       <div className="chat-description">
         <h1>Veja o FanBot em <br/> ação!</h1>
         <video
-          src="../../assets/chat-video.mp4"
+          src={Video}
           controls
           width="600"
         ></video>
@@ -50,7 +52,7 @@ const HowToUse = () => {
           <li ref={addToRefs}><span>✔️</span> Curiosidades, estatísticas e interatividade</li>
         </ul>
       </div>
-      <img ref={imgRef} className="animated-image"  src="../../assets/chat.jpg" alt="" />
+      <img ref={imgRef} className="animated-image"  src={Image} alt="Chatbot Image" />
     </section>
   );
 };
